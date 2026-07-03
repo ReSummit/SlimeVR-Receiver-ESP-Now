4c1841eb6cf99818137c64b446d788f60229719b
ReSummit <dmvo@ucsd.edu>
2026-07-03 01:58:06 -0700

    Forgot a define call for SERIAL_USB_ONLY

--- src/Serial.cpp
+++ src/Serial.cpp
@@ -1,6 +1,6 @@
 #include "Serial.h"
 
-#if !(defined(ARDUINO_USB_CDC_ON_BOOT) && ARDUINO_USB_CDC_ON_BOOT) && ARDUINO_USB_MODE && !defined(SERIAL_USB_ONLY)
+#if !(defined(ARDUINO_USB_CDC_ON_BOOT) && ARDUINO_USB_CDC_ON_BOOT) && defined(ARDUINO_USB_MODE) && !defined(SERIAL_USB_ONLY)
 USBCDC USBSerial;
 HybridSerial Serial;
 #else
